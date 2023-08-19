f39a48b64cee645219cac9efeda939a56732dcd5
Ujjwal Sinha <sinhaujjwal04@gmail.com>
2023-08-19 18:53:30 +0530

    newChange

--- src/App.js
+++ src/App.js
@@ -1,40 +1,33 @@
 import React from 'react';
-//import keyConceptsImage from './assets/images/key-concepts.png';
-
-
+import CYngH5 from  './assets/images/CYngH5.jpg';
+import InputForm from './Components/FormTag';
+import './App.css';
 const concepts = [
   {
     orderId: '#123',
     amount : 12,
-    date : new Date(2023,8,18)
-  }
+    date : new Date(2023,8,18),
+  },
   {
     orderId: '#124',
     amount : 10,
-    date : new Date(2023,7,18)
+    date : new Date(2023,7,18),
   },
   {
     orderId: '#125',
     amount : 14,
-    date : new Date(2023,6,18)
+    date : new Date(2023,6,18),
   },
 ];
 
 function App() {
   return (
-    <div>
-      <header>
-        <img src={keyConceptsImage} alt="Medal badge with a star" />
-        <h1>Key React Concepts</h1>
-        <p>Selected key React concepts you should know about</p>
-      </header>
-      <ul id="concepts">
-        <li className="concept">
-          <img src="TODO: IMAGE" alt="TODO: TITLE" />
-          <h2>TODO: TITLE</h2>
-          <p>TODO: DESCRIPTION</p>
-        </li>
-      </ul>
+    <div className='bg'>
+      
+         <img src={CYngH5} alt="coffe bean background" />
+        <InputForm></InputForm>
+    
+      
     </div>
   );
 }
---
+++ src/Components/FormTag.js
@@ -0,0 +1,89 @@
+import './FormTag.css';
+import React,{useState} from 'react';
+function FormTag(props)
+{
+     const[enteredTitle,setEnteredTitle]=useState('');
+     const[enteredAmount,setEnteredAmount]=useState('');
+     const[enteredDate,setEnteredDate]=useState('');
+    
+    function titleChangeHandler(event)
+    {
+         setEnteredTitle(event.target.value);
+
+     }
+    function amountChangeHandler(event)
+    {
+        setEnteredAmount(event.target.value);
+        
+    }
+    function dateChangeHandler(event)
+    {
+        console.log(event.target.value);
+         setEnteredDate(event.target.value);
+    
+    }
+    
+    function submitHandler(event)
+    {
+           event.preventDefault();
+           const pushData={
+            title:enteredTitle,
+            amount:+enteredAmount,
+            date:new Date(enteredDate),
+           };
+         
+         props.onSaveExpenseDetails(pushData);
+           setEnteredTitle('');
+           setEnteredAmount('');
+           setEnteredDate('');
+           setShowForm(false);
+           setButtonShow(true);
+        
+    }
+    const[showForm,setShowForm]=useState(false);
+    const[buttonShow,setButtonShow]=useState(true);
+    function formDiplay()
+    {
+        setShowForm(true);
+        setButtonShow(false);
+    }
+    function CloseForm()
+    {
+        setShowForm(false);
+        setButtonShow(true);
+    }
+
+
+
+  return(
+    <div>
+    {showForm && (<form onSubmit= {submitHandler}>
+        
+        <div className='new-expense__controls'>
+            <div className='new-expense__control'>
+                <label>Title</label>
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler}/>
+            </div>
+            <div className='new-expense__control'>
+                <label>Amount</label>
+                <input type='number' min="0.01" step="0.01" value={enteredAmount} onChange={amountChangeHandler}/>
+            </div>
+            <div className='new-expense__control'> 
+                <label>Date</label>
+                <input type='date' min="2022-01-01" max="2024-12-31" value={enteredDate} onChange={dateChangeHandler}/>
+            </div>
+        </div>
+       
+        <div className='new-expense__actions '>
+         <button  onClick={CloseForm}>Cancel</button> 
+        <button type='submit'>Add Expense</button>  
+ 
+        </div>
+       
+    </form>)}
+    {buttonShow && (<button onClick={formDiplay}>Add New Expenses</button>)};
+    </div> 
+   
+  );
+  }
+export default FormTag;
